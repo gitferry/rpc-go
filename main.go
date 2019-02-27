9d35d044b1ec24ee826abe5d9928ba55991e778b
greferry <greferry@gmail.com>
2019-02-26 23:38:10 -0800

    Modified functions to methods

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+)
 
 type Item struct {
 	title string
 	body  string
 }
 
+type API int
+
 var database []Item
 
-func GetItemByName(title string) Item {
+func (a *API) GetItemByName(title string, reply *Item) error {
 	var returnedItem Item
 
 	for _, val := range database {
@@ -18,28 +25,32 @@ func GetItemByName(title string) Item {
 		}
 	}
 
-	return returnedItem
+	*reply = returnedItem
+	return nil
 }
 
-func AddItem(item Item) Item {
+func (a *API) AddItem(item Item, reply *Item) error {
 	database = append(database, item)
-	return item
+
+	*reply = item
+	return nil
 }
 
-func EditItem(title string, edit Item) Item {
+func (a *API) EditItem(edit Item, reply *Item) error {
 	var changedItem Item
 
 	for idx, val := range database {
-		if val.title == title {
+		if val.title == edit.title {
 			database[idx] = edit
 			changedItem = edit
 		}
 	}
 
-	return changedItem
+	*reply = changedItem
+	return nil
 }
 
-func DeleteItem(item Item) Item {
+func (a *API) DeleteItem(item Item, reply *Item) error {
 	var deletedItem Item
 
 	for idx, val := range database {
@@ -50,28 +61,51 @@ func DeleteItem(item Item) Item {
 		}
 	}
 
-	return deletedItem
+	*reply = deletedItem
+	return nil
 }
 
 func main() {
-	fmt.Println("initial database: ", database)
-	a := Item{"first", "first test item"}
-	b := Item{"second", "second test item"}
-	c := Item{"third", "third test item"}
+	// fmt.Println("initial database: ", database)
+	// a := Item{"first", "first test item"}
+	// b := Item{"second", "second test item"}
+	// c := Item{"third", "third test item"}
 
-	AddItem(a)
-	AddItem(b)
-	AddItem(c)
-	fmt.Println("second database: ", database)
+	// AddItem(a)
+	// AddItem(b)
+	// AddItem(c)
+	// fmt.Println("second database: ", database)
 
-	DeleteItem(b)
-	fmt.Println("third database: ", database)
+	// DeleteItem(b)
+	// fmt.Println("third database: ", database)
 
-	EditItem("third", Item{"forth", "forth test item"})
-	fmt.Println("forth database: ", database)
+	// EditItem("third", Item{"forth", "forth test item"})
+	// fmt.Println("forth database: ", database)
 
-	x := GetItemByName("forth")
-	y := GetItemByName("first")
-	fmt.Println(x, y)
+	// x := GetItemByName("forth")
+	// y := GetItemByName("first")
+	// fmt.Println(x, y)
+
+	var api = new(API)
+	err := rpc.Register(api)
+
+	if err != nil {
+		log.Fatal("error registering API", err)
+	}
+
+	rpc.HandleHTTP()
+
+	listener, err := net.Listen("tcp", ":4040")
+
+	if err != nil {
+		log.Fatal("Listener error", err)
+	}
+
+	log.Printf("serving rpc on port %d", 4040)
+	err = http.Serve(listener, nil)
+
+	if err != nil {
+		log.Fatal("error serving: ", err)
+	}
 
 }
